3e87e7bfac02a47211069ecf07090d1a0270723a
Md.Monirul Islam <smahkafi>
2022-05-25 15:45:58 +0600

    header part done

---
+++ js.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+  document.querySelector(".starbucks").scr = anything;
+}
+function changeCircleColor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
